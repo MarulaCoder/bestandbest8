7f8c5550d71914cb921adfccf33b3e49be03975f
MarulaCoder <45479929+MarulaCoder@users.noreply.github.com>
2020-06-02 21:36:05 +0200

    Added better views and sidenav

--- controllers/tweetController.js
+++ controllers/tweetController.js
@@ -6,7 +6,15 @@ const { body, validationResult } = require('express-validator');
 const { sanitizeBody } = require('express-validator');
 
 exports.sendTweet = (req, res, next) => {
-    bizOps.readFile(req, res, next);
+    if(req.body.isOnceOffTweet)
+    {
+        bizOps.sendSingleTweet(req, res, next);
+    }
+    if(!req.body.isOnceOffTweet)
+    {
+        bizOps.sendThreadTweet(req, res, next);
+    }
+    
 }
 
 exports.getAllTweets = (req, res, next) => {
